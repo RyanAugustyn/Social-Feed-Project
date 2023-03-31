ab97a5d00c03bb72c6d58c9c866ac14bd76694ec
Ryan Augustyn <jraugustyn07@gmail.com>
2023-03-31 16:50:59 -0500

    initial files

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,25 @@
+//username, body of post, and like/dislike buttons
+
+import React, { useState } from 'react';
+
+
+const Post = (props) => {
+
+    const [userName, setUserName] = useState('');
+    const [textBody, setTextBody] = useState('');
+
+    <form>
+        <div>
+            <h3>Name</h3>
+            <input type='text'
+        </div>
+    </form>
+
+
+
+    return ( 
+
+     );
+}
+ 
+export default Post;
---
+++ src/Components/PostList/PostList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const PostList = (props) => {
+    return ( 
+        <h1>Posts Here</h1>>
+     );
+}
+ 
+export default PostList;
+
