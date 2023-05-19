7ce6dbb2ce66919a79b87bb350f471957903a38b
Ryan Augustyn <jraugustyn07@gmail.com>
2023-05-19 11:41:11 -0500

    changed == to === in button files

--- src/Components/DislikeBtn/DislikeBtn.jsx
+++ src/Components/DislikeBtn/DislikeBtn.jsx
@@ -1,31 +1,28 @@
-import React, { useState } from 'react';
-import './DislikeBtn.css'
+import React, { useState } from "react";
+import "./DislikeBtn.css";
 
 const DislikeBtn = (props) => {
-    const [color, setColor] = useState(false);
+  const [color, setColor] = useState(false);
 
-    
-    function handleClick(){
-        props.setDislikeStatus(!props.dislikeStatus);
-        if(props.likeStatus){
-            props.setLikeStatus(false);
-        }
-        if(color == ''){
-            setColor('dislikeBtn');
-        }
-        else if(color == 'dislikeBtn'){
-            setColor('');
-        }
+  function handleClick() {
+    props.setDislikeStatus(!props.dislikeStatus);
+    if (props.likeStatus) {
+      props.setLikeStatus(false);
     }
+    if (color === "") {
+      setColor("dislikeBtn");
+    } else if (color === "dislikeBtn") {
+      setColor("");
+    }
+  }
 
-    return ( 
-        <div>
-            <button className='btn btn-primary' id={color}
-            onClick={handleClick}>
-                {props.dislikeStatus ? "DISLIKED!!!" : "Dislike?"}
-            </button>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <button className="btn btn-primary" id={color} onClick={handleClick}>
+        {props.dislikeStatus ? "DISLIKED!!!" : "Dislike?"}
+      </button>
+    </div>
+  );
+};
+
 export default DislikeBtn;
--- src/Components/LikeBtn/LikeBtn.jsx
+++ src/Components/LikeBtn/LikeBtn.jsx
@@ -1,31 +1,28 @@
-import React, { useState } from 'react';
-import './LikeBtn.css'
+import React, { useState } from "react";
+import "./LikeBtn.css";
 
 const LikeBtn = (props) => {
-    const [color, setColor] = useState(false);
+  const [color, setColor] = useState(false);
 
-
-    function handleClick(){
-        props.setLikeStatus(!props.likeStatus);
-        if(props.dislikeStatus){
-            props.setDislikeStatus(false);
-        }
-        if(color == ''){
-            setColor('likeBtn');
-        }
-        else if(color == 'likeBtn'){
-            setColor('');
-        }
+  function handleClick() {
+    props.setLikeStatus(!props.likeStatus);
+    if (props.dislikeStatus) {
+      props.setDislikeStatus(false);
     }
+    if (color === "") {
+      setColor("likeBtn");
+    } else if (color === "likeBtn") {
+      setColor("");
+    }
+  }
 
-    return ( 
-        <div className='likeBtn'>
-            <button className='btn btn-primary' id={color}
-            onClick={handleClick}>
-                {props.likeStatus ? "LIKED!" : "Like?"}
-            </button>
-        </div>
-     );
-}
- 
+  return (
+    <div className="likeBtn">
+      <button className="btn btn-primary" id={color} onClick={handleClick}>
+        {props.likeStatus ? "LIKED!" : "Like?"}
+      </button>
+    </div>
+  );
+};
+
 export default LikeBtn;
